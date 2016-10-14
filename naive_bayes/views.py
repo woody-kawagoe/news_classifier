e24934fca5520f10bc98997ef3151ac618faede5
woody-kawagoe <hip.hop.zip.please@gmail.com>
2016-10-14 11:17:41 +0900

    modify by pep8

--- naive_bayes/views.py
+++ naive_bayes/views.py
@@ -4,34 +4,36 @@ from django.shortcuts import render
 from django.urls import reverse
 import pandas as pd
 
+
 def form(request):
     return render(request, 'naive_bayes/form.html')
 
+
 def result(request):
-    url=request.POST['url']
-    p=Popen(["python","trainer/getBoW.py",url],stdout=PIPE)
-    c=p.stdout.readlines()
+    url = request.POST['url']
+    p = Popen(["python", "trainer/getBoW.py", url], stdout=PIPE)
+    c = p.stdout.readlines()
     if c:
-        bow=c[0].decode('utf-8').split(" ")
+        bow = c[0].decode('utf-8').split(" ")
     else:
-        bow=[]
-    classifier=pd.read_csv("trainer/classifier.csv",index_col=0)
-    categories=classifier.columns
-    P={}
+        bow = []
+    classifier = pd.read_csv("trainer/classifier.csv", index_col=0)
+    categories = classifier.columns
+    P = {}
     for name in categories:
-        P[name]=0.
+        P[name] = 0.
     for word in bow:
         for name in categories:
             if word in classifier.index:
-                P[name]+=classifier.ix[word,name]
-    P=sorted(P.items(),key=lambda x:x[1],reverse=True)
+                P[name] += classifier.ix[word, name]
+    P = sorted(P.items(), key=lambda x: x[1], reverse=True)
     if P[0][1] is 0.0:
-        category="Error"
+        category = "Error"
     else:
-        category=P[0][0]
-    context={
-        'P':P,
-        'category':category,
-        'url':url
+        category = P[0][0]
+    context = {
+        'P': P,
+        'category': category,
+        'url': url
         }
     return render(request, 'naive_bayes/result.html', context)
--- trainer/getNews.py
+++ trainer/getNews.py
@@ -2,41 +2,42 @@
 from subprocess import Popen, PIPE
 import os.path
 
-categories=[
-    ["1","エンタメ"],
-    ["2","スポーツ"],
-    ["3","おもしろ"],
-    ["4","国内"],
-    ["5","海外"],
-    ["6","コラム"],
-    ["7","IT・科学"],
-    ["8","グルメ"]
+categories = [
+    ["1", "エンタメ"],
+    ["2", "スポーツ"],
+    ["3", "おもしろ"],
+    ["4", "国内"],
+    ["5", "海外"],
+    ["6", "コラム"],
+    ["7", "IT・科学"],
+    ["8", "グルメ"]
 ]
 
+
 def getNews(newslist):
-    news=""
+    news = ""
     for news_url in newslist:
-        p=Popen(["python","trainer/getBoW.py",news_url],stdout=PIPE)
-        c=p.stdout.readlines()
+        p = Popen(["python", "trainer/getBoW.py", news_url], stdout=PIPE)
+        c = p.stdout.readlines()
         if c:
-            bow=c[0].decode('utf-8')
+            bow = c[0].decode('utf-8')
         else:
-            bow=""
-        print(news_url,bow[:10],"...")
-        news=news+bow+"\n"
+            bow = ""
+        print(news_url, bow[:10], "...")
+        news = news+bow+"\n"
     return news
 
-for id,name in categories:
-    print(id,name)
-    path='trainer/newslist/'+name+'.csv'
+for id, name in categories:
+    print(id, name)
+    path = 'trainer/newslist/'+name+'.csv'
     if os.path.exists(path):
-        newslist=list(map(lambda x:x[:-1],open(path,'r').readlines()))
+        newslist = list(map(lambda x: x[:-1], open(path, 'r').readlines()))
     else:
-        print("not found!",path)
-    path='trainer/news/'+name+'.csv'
+        print("not found!", path)
+    path = 'trainer/news/'+name+'.csv'
     if os.path.exists(path):
-        if len(open(path,'r').readlines()) is 100:
-            print("already get",name,"100 news")
+        if len(open(path, 'r').readlines()) is 100:
+            print("already get", name, "100 news")
     else:
-        news=getNews(newslist)
-        open(path,'w').write(news)
+        news = getNews(newslist)
+        open(path, 'w').write(news)
